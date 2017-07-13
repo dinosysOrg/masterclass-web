b8ad582e3635a16881f91dde59d0d14c6ef32a7f
nhatdev1193 <nhat.nguyen@dinosys.vn>
2017-07-13 10:23:37 +0700

    add index for app

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+/**
+ * Main Class of project
+ */
+export default class App extends React.Component {
+/**
+ * render app 
+ * @return {html} The template of App class
+ */
+  render() {
+    return (
+      <div>
+        <h1> MasterClass </h1>
+      </div>
+    );
+  }
+}
