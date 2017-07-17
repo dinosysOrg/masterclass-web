2303dc8b74a4756d763ae328c7ef2b2b6b15e0d1
nhatdev1193 <nhat.nguyen@dinosys.vn>
2017-07-17 17:37:37 +0700

    implement react router server rendering in development

--- server/index.dev.js
+++ server/index.dev.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {renderToString} from 'react-dom/server';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
+import {StaticRouter} from 'react-router-dom';
 import webpack from 'webpack';
 import config from '../webpack.config.dev.js';
 import App from '../src/app';
@@ -17,15 +18,23 @@ app.use(webpackDevMiddleware(compiler, {
 }));
 app.use(webpackHotMiddleware(compiler));
 app.get('*', (req, res) => {
+  const context = {};
+  const html = renderToString(
+    <StaticRouter location={req.url} context={context}>
+      <App/>
+    </StaticRouter>
+  );
+
+  if (context.url) res.redirect(context.url);
+
   res.send(`
       <!DOCTYPE html>
       <head>
         <title>MasterClass Platform</title>
         <link rel="stylesheet" href="assets/css/bundle.css">
       </head>
-
       <body>
-        <div id="app"><div>${renderToString(<App />)}</div></div>
+        <div id="app"><div>${html}</div></div>
         <script src="assets/js/bundle.js"></script>
       </body>
     </html>
