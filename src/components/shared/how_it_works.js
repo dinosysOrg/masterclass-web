918f7e5bea9e745b3025219680395cae2d81e5ff
Khoa Do <happyboy0412@gmail.com>
2017-07-18 11:38:20 +0700

    build static home page

---
+++ src/components/shared/how_it_works.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Panel from '../shared/panel';
+
+export default () => {
+  return (
+    <Panel title="How It Works">
+      <ul>
+        <li>
+          <img src="#" alt="" />
+          <p>Enter a form about your musical interest</p>
+        </li>
+        <li>
+          <img src="#" alt="" />
+          <p>Use filter options to choose a path that fits you most</p>
+        </li>
+        <li>
+          <img src="#" alt="" />
+          <p>Save the videos to a path on your dashboard</p>
+        </li>
+        <li>
+          <img src="#" alt="" />
+          <p>Get feedback directly from accredited teachers</p>
+        </li>
+      </ul>
+
+      <a href="#">Discovery Now</a>
+    </Panel>
+  );
+};
