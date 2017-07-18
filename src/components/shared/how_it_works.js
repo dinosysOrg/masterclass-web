e00f3432ef1f1a3fd927e20febcf0284088c7d5c
Khoa Do <happyboy0412@gmail.com>
2017-07-18 14:21:29 +0700

    add css

--- src/components/shared/how_it_works.js
+++ src/components/shared/how_it_works.js
@@ -5,26 +5,26 @@ import Panel from '../shared/panel';
 export default () => {
   return (
     <Panel title="How It Works">
-      <ul>
-        <li>
+      <ul className="row">
+        <li className="col s12 m3 center-align">
           <img src="#" alt="" />
           <p>Enter a form about your musical interest</p>
         </li>
-        <li>
+        <li className="col s12 m3 center-align">
           <img src="#" alt="" />
           <p>Use filter options to choose a path that fits you most</p>
         </li>
-        <li>
+        <li className="col s12 m3 center-align">
           <img src="#" alt="" />
           <p>Save the videos to a path on your dashboard</p>
         </li>
-        <li>
+        <li className="col s12 m3 center-align">
           <img src="#" alt="" />
           <p>Get feedback directly from accredited teachers</p>
         </li>
       </ul>
 
-      <a href="#">Discovery Now</a>
+      <a className="btn" href="#">Discovery Now</a>
     </Panel>
   );
 };
