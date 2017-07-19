26273bc2d0a7105b82d9d4226baebfa74d15e464
Khoa Do <happyboy0412@gmail.com>
2017-07-19 14:46:36 +0700

    add implement of browse page

--- src/containers/browse/browse.js
+++ src/containers/browse/browse.js
@@ -1,4 +1,9 @@
 import React, {Component} from 'react';
+
+import FullVideoCatalog from '../../components/shared/full_video_catalog';
+import VideoPanel from '../../components/shared/video_panel';
+import PromoPanel from '../../components/shared/promo_panel';
+
 /**
  * Browse of project
  */
@@ -11,32 +16,11 @@ class Browse extends Component {
     return (
       <div className="browse-page">
         <div className="container">
-          <section>
-            <div className="row">
-              <div className="col center s12">
-                <h3>
-                  <i className="mdi-content-send brown-text" />
-                </h3>
-                <h4>Browse Page</h4>
-                <p className="left-align light">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam
-                  pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo,
-                  ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin
-                  lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi
-                  massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat.
-                  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-                </p>
-                <p className="left-align light">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam
-                  pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo,
-                  ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin
-                  lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi
-                  massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat.
-                  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-                </p>
-              </div>
-            </div>
-          </section>
+          <PromoPanel />
+          <FullVideoCatalog />
+          <VideoPanel title="Popular Paths" />
+          <VideoPanel title="Lastest Paths" />
+          <PromoPanel />
         </div>
       </div>
     );
