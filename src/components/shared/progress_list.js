8fe12c1b7585b16e3e20de77e543776e1b0ac2ff
Khoa Do <happyboy0412@gmail.com>
2017-07-19 10:13:31 +0700

    add implement of dashboard

---
+++ src/components/shared/progress_list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default ({title}) => {
+  return (
+    <div>
+      <h5>{title}</h5>
+      <ul>
+        <li>x</li>
+        <li>x</li>
+        <li>o</li>
+        <li>o</li>
+        <li>_</li>
+      </ul>
+    </div>
+  );
+};
