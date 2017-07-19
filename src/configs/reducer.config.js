585495707bb9cc3bb0b76eafcee73af8841fcd27
votheduy154 <votheduy154@gmail.com>
2017-07-19 15:21:58 +0700

    fix reducer

--- src/configs/reducer.config.js
+++ src/configs/reducer.config.js
@@ -1,9 +1,7 @@
 import {combineReducers} from 'redux';
 import {initReducer} from '../redux';
 
-const rootReducer = combineReducers({
-  initStore: initReducer,
-});
+const rootReducer = combineReducers({initReducer});
 export default rootReducer;
 
 
