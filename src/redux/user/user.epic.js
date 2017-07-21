e33c52ad1f3fa1d101d626f3d1a8db1a4de41f2e
votheduy154 <votheduy154@gmail.com>
2017-07-21 16:59:43 +0700

    edit api

--- src/redux/user/user.epic.js
+++ src/redux/user/user.epic.js
@@ -1,8 +1,8 @@
-// import { FETCHING_DATA } from './constants'
-import * as types from './user.types';
-import * as actions from './user.action';
 import 'rxjs';
 import {ajax} from 'rxjs/observable/dom/ajax';
+import * as types from './user.types';
+import * as actions from './user.action';
+import {getUserAPI} from './user.api';
 
 /**
  * action fetch data
@@ -12,7 +12,7 @@ import {ajax} from 'rxjs/observable/dom/ajax';
 const fetchUserEpic = (action$) =>
   action$.ofType(types.FETCHING_USER)
     .mergeMap((action) =>
-      ajax.getJSON(`https://randomuser.me/api/?results=100`)
+      ajax.getJSON(getUserAPI)
         .map((response) => actions.fetchUserSuccess(response))
         .catch((error) => actions.fetchUserFailure(error))
     );
