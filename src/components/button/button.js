3a61083dacf40e1e52e97d7a606377d3db0c60c6
votheduy154 <votheduy154@gmail.com>
2017-08-21 10:54:53 +0700

    add QUIZ UI

---
+++ src/components/button/button.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+/**
+ * Button of project
+ */
+class Button extends Component {
+  /**
+ * render Button
+ * @return {html} The template of Button class
+ */
+  render() {
+    return (
+      <div className="button-VMA">
+        <a href="#">
+          {child}
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Button;
