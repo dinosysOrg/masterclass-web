5daa4c1d9d3e5c19311361b533622d681d81b2d4
Duy Nguyễn <batuimatui@gmail.com>
2017-08-28 02:41:41 +0700

    sidebar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -4,21 +4,44 @@ import React, {Component} from 'react';
  */
 class Siderbar extends Component {
   /**
+   * render QuizLoading template
+   * @return {html} The template of menu class
+  */
+  checkSidebar() {
+    const {pathname} = this.props.location;
+    if (pathname === '/Profile') {
+      return (
+        <div className="sidebar">
+          <ul className="nav nav-pills flex-column">
+            <li className="nav-item"><a href="">General Information</a></li>
+            <li className="nav-item"><a href="">Settings</a></li>
+          </ul>
+        </div>
+      );
+    }
+    if (pathname === '/Dashboard') {
+      return (
+        <div className="sidebar">
+          <ul className="nav nav-pills flex-column">
+            <li className="nav-item"><a href="">Overview</a></li>
+            <li className="nav-item"><a href="">Syllabus</a></li>
+            <li className="nav-item"><a href="">Assignment</a></li>
+            <li className="nav-item"><a href="">Pratice</a></li>
+            <li className="nav-item"><a href="">Tuner</a></li>
+            <li className="nav-item"><a href="">Q&A</a></li>
+          </ul>
+        </div>
+      );
+    }
+  }
+  /**
    * render Siderbar template
    * @return {html} The template of menu class
    */
   render() {
+    console.log(this.props);
     return (
-      <div className="sidebar">
-        <ul>
-          <li><a href="">Overview</a></li>
-          <li><a href="">Syllabus</a></li>
-          <li><a href="">Assignment</a></li>
-          <li><a href="">Pratice</a></li>
-          <li><a href="">Tuner</a></li>
-          <li><a href="">Q&A</a></li>
-        </ul>
-      </div>
+      this.checkSidebar()
     );
   }
 }
