cb2b722df0c1a25a7180f1e81f7b449b60841da2
Hiep Nguyen <ngochiep9523@gmail.com>
2017-10-02 14:44:08 +0700

    add download

--- src/redux/index.js
+++ src/redux/index.js
@@ -4,7 +4,7 @@ import {loginRequestEpic, signupRequestEpic, fbRequestEpic, myPathRequestEpic, g
   getQuizRequestEpic, saveQuizEpic, putUserInfoRequestEpic, putUserLayoutEpic} from './user/user.epic';
 import {changeEnEpic, changeViEpic} from './init/init.epic';
 import pathReducer from './path/path.reducer';
-import {getBrowsePath, getHomePath, searchPath, overviewPath, fetchInstrument, fetchCourses, subscribePath, unsubscribePath, fetchOverallPath, getPracticeEpic} from './path/path.epic';
+import {getBrowsePath, getHomePath, searchPath, overviewPath, fetchInstrument, fetchCourses, subscribePath, unsubscribePath, fetchOverallPath, getPracticeEpic, getSheetEpic} from './path/path.epic';
 import syllabusReducer from './syllabus/syllabus.reducers';
 import {getSyllabusDetailEpic} from './syllabus/syllabus.epic'
 
@@ -35,4 +35,5 @@ export {
   unsubscribePath,
   fetchOverallPath,
   getPracticeEpic,
+  getSheetEpic,
 };
